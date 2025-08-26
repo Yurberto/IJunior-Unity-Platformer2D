3dd1e64e9ce73af0095f0ee63f7101e48f1be60f
Yurberto <sta1993lone@gmail.com>
2025-08-26 12:47:34 +0300

    Delete class Raycaster

--- Assets/Scripts/Raycaster.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Raycaster : MonoBehaviour
-{
-    public RaycastHit2D CastRay(Vector2 current, Vector2 target, float maxDistance)
-    {
-        return Physics2D.Raycast(current, target, maxDistance);
-    }
-}
