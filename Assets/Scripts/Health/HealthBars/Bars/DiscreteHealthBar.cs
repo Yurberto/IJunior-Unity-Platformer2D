652da65a8cf4268028b37df8ddf1de059050c910
Yurberto <sta1993lone@gmail.com>
2025-09-22 16:55:37 +0300

    Delete unnecessary scripts

--- Assets/Scripts/Health/HealthBars/Bars/DiscreteHealthBar.cs
+++
@@ -1,7 +0,0 @@
-public class DiscreteHealthBar : ImageHealthBar
-{
-    protected override void UpdateHealthData()
-    {
-        FillZone.ApplyFill(Health.CurrentValue / Health.MaxValue);
-    }
-}
